405710f1aa9dfb94b16849e619da2a34fc462c4c
hollc <380460708@qq.com>
2023-01-30 16:31:26 +0800

    精简频道名中表情，优化保存文件夹名称

--- tools/down_file.py
+++ tools/down_file.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+import demoji
 from telethon import TelegramClient
 from telethon.tl import types
 
@@ -9,7 +10,7 @@ from tools.tool import shorten_filename
 from tools.tqdm import TqdmUpTo
 
 
-async def download_file(channel_title, message):
+async def download_file(channel_title, channel_id, message):
     # 获取媒体类型
     is_webpage = isinstance(message.media, types.MessageMediaWebPage)
     is_photo = isinstance(message.media, types.MessageMediaPhoto)
@@ -24,14 +25,15 @@ async def download_file(channel_title, message):
             message.message = str(message.photo.id)
         else:
             message.message = str(message.document.id)
-    channel_title += '/'
+    # channel_title += '/'
     message.message = shorten_filename(message.message)
     if is_photo:
         file_type = 'jpg'
     else:
         file_type = message.media.document.mime_type.split('/')[-1]
     file_name = message.message + f'.{file_type}'
-    long_path = os.environ['save_path'] + channel_title + file_name
+    # long_path = os.environ['save_path'] + channel_title + file_name
+    long_path = f'{os.environ["save_path"]}/{channel_title}-{channel_id}/{message.message}.{file_type}'
     if not os.path.exists(long_path) or (is_doc and os.path.getsize(long_path) != message.media.document.size) or (
             is_photo and hasattr(message.media.photo.sizes, 'sizes') and os.path.getsize(long_path) !=
             message.media.photo.sizes[-1].sizes[-1] or is_photo and hasattr(message.media.photo.sizes,
@@ -54,11 +56,15 @@ async def download_file(channel_title, message):
 
 async def down_group(client: TelegramClient, chat_id, plus_func: str):
     # 检测chat_id是id还是昵称
-    isId = re.match(r'-?[1-9][0-9]{4,}',chat_id)
+    isId = re.match(r'-?[1-9][0-9]{4,}', chat_id)
     if isId is None:
         entity = await client.get_entity(chat_id)
         chat_id = entity.id
     channel_title, messages = await tools.tool.getHistoryMessage(client, int(chat_id))
+    # 频道名称中的表情转文字，以兼容不同字符集设备
+    channel_title = demoji.replace(channel_title, '[emoji]')
+    # Todo 为了应对某些频道改名导致存储路径更新，通过chat_id预处理文件夹名称
+    # 识别到存在相同id文件夹时，更新旧文件夹名称
     async for message in messages:
         # 0表示不执行操作，1表示continue，2表示break
         switch = tools.tool.can_continue(message.id, plus_func)
@@ -73,5 +79,5 @@ async def down_group(client: TelegramClient, chat_id, plus_func: str):
 
             # await client.send_message(-1001877389961, message)
             # await message.forward_to(-1001877389961)
-            await download_file(channel_title, message)
+            await download_file(channel_title, chat_id, message)
     print(channel_title, '全部下载完成')
