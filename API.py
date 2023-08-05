5768b4bce42ca74a727bba9740437d7ded98e32e
hollc <380460708@qq.com>
2023-08-05 22:46:22 +0800

    删除官方API

--- API.py
+++
@@ -1,6 +0,0 @@
-# 获取最近消息
-getUpdates_api = 'https://api.telegram.org/bot%s/getUpdates'
-# 发送消息
-sendMessage_api = 'https://api.telegram.org/bot%s/sendMessage'
-
-forwardMessage_api ='https://api.telegram.org/bot%s/forwardMessage'
