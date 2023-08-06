31ae1901ebf1d72f04dcb95a10d09ee96c1cf843
hollc <380460708@qq.com>
2023-08-06 17:10:29 +0800

    优化上传（非视频文件不再报错）

--- tools/upload_file.py
+++ tools/upload_file.py
@@ -34,9 +34,13 @@ async def upload_file(client: TelegramClient, chat_id, path: str, del_after_uplo
         file_caption = os.path.basename(file_path)
         file_size = os.path.getsize(file_path)
         # 发送文件到指定的群组或频道
-        try:
-            with TqdmUpTo(total=file_size, desc=file_caption) as bar:
+        isVideo = True
+        with TqdmUpTo(total=file_size, desc=file_caption) as bar:
+            try:
                 thumb_input = await client.upload_file(BytesIO(GetThumb(file_path)))
+            except OSError:
+                isVideo = False
+            if isVideo:
                 # 获取视频文件的时长
                 video_duration = int(VideoFileClip(file_path).duration)
                 # 获取视频文件的宽度和高度
@@ -51,24 +55,23 @@ async def upload_file(client: TelegramClient, chat_id, path: str, del_after_uplo
                     round_message=False,
                     supports_streaming=True
                 )
-
-                # 上传文件到Telegram服务器
-                try:
-                    result = await client.upload_file(file_path, progress_callback=bar.update_to)
-                except RuntimeError:
-                    print(f'上传出错，跳过{file_caption}')
-                    continue
-                await client.send_file(
-                    peo,
-                    result,
-                    caption=file_caption.rsplit('.', maxsplit=1)[0],
-                    thumb=thumb_input,
-                    progress_callback=bar.update_to,
-                    attributes=[video_attr])
-                if del_after_upload:
-                    os.remove(file_path)
-        except CancelledError:
-            print("取消上传")
-            sys.exit()
-        # except Exception as e:
-        #     print("上传出错", e.__class__.__name__)
+            # 上传文件到Telegram服务器
+            try:
+                result = await client.upload_file(file_path, progress_callback=bar.update_to)
+            except RuntimeError:
+                print(f'上传出错，跳过{file_caption}')
+                continue
+            except CancelledError:
+                print("取消上传")
+                sys.exit()
+            await client.send_file(
+                peo,
+                result,
+                caption=file_caption.rsplit('.', maxsplit=1)[0],
+                thumb=thumb_input if isVideo else None,
+                progress_callback=bar.update_to,
+                attributes=[video_attr] if isVideo else None)
+            if del_after_upload:
+                os.remove(file_path)
+    # except Exception as e:
+    #     print("上传出错", e.__class__.__name__)
